d7db6dfdd27077b99a982c696b2c2a9247413625
seoseongjo <tjtjdwh5321@gmail.com>
2022-04-24 23:53:49 +0900

    node 환경에서 http로 서버 열었을 때 TypeScript로 문제 찾기

--- main.js
+++ main.js
@@ -4,7 +4,16 @@
 // Formatting : Linting, Type Checking
 // Formatting : Prettier
 // Linting : ESLint
+// Type checking : TypeScript
 
-const someString = 'Hello'
-const result = Math.log(someString)
-console.log(result)
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.end('Hello!')
+})
+
+const PORT = 4000
+server.listen(PORT, () => {
+  console.log(`The server is Listening at port : ${PORT}.`)
+})
